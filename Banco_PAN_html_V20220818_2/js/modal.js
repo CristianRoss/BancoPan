c694984a1b24c34e4e63c7a4ce85cb8a76ef9807
Wendel Eduardo <71601127+WendelEduardo@users.noreply.github.com>
2022-08-18 16:52:24 -0300

    Add files via upload

---
+++ Banco_PAN_html_V20220818_2/js/modal.js
@@ -0,0 +1,26 @@
+
+
+function alerta(){
+  let timerInterval
+Swal.fire({
+  title: 'Carregando base de Dados!',
+  html: 'Sua base ficara pronta em: ',
+  timer: 20000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 1000)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+}
---
+++ Banco_PAN_html_V20220818_2/js/script.js
@@ -0,0 +1,7 @@
+
+// let card = document.getElementsByClassName('card');
+// let descricao = document.getElementsByClassName('card__title');
+
+// card.addEventListener("mouseover", () => {
+//   alert("Alert");
+// });
