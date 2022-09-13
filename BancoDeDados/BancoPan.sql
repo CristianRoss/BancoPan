72192ab41aa4bd076ba5faddd1e1eef12272bce1
Cristian Rossmann <99340448+CristianRoss@users.noreply.github.com>
2022-09-13 14:57:28 -0300

    Update BancoPan.sql

--- BancoDeDados/BancoPan.sql
+++ BancoDeDados/BancoPan.sql
@@ -1,5 +1,5 @@
 -- Gerado por Oracle SQL Developer Data Modeler 22.2.0.165.1149
---   em:        2022-09-13 14:08:03 BRT
+--   em:        2022-09-13 14:27:45 BRT
 --   site:      Oracle Database 11g
 --   tipo:      Oracle Database 11g
 
@@ -12,10 +12,10 @@
 CREATE TABLE cartao_de_credito (
     id_cartao_credito INTEGER NOT NULL,
     numero            INTEGER NOT NULL,
-    fatura            NUMBER(50, 2) NOT NULL,
+    fatura            NUMBER(38, 2) NOT NULL,
     data_vencimento   DATE NOT NULL,
     juros_credito     NUMBER(6, 2) NOT NULL,
-    limite            NUMBER(50, 2),
+    limite            NUMBER(38, 2),
     id_conta_jur      INTEGER,
     id_conta_fisica   INTEGER
 );
@@ -33,7 +33,7 @@ ALTER TABLE cartao_de_credito ADD CONSTRAINT credito_numero_uk UNIQUE ( numero )
 CREATE TABLE cartao_debito (
     id_cartao_debito INTEGER NOT NULL,
     numero           INTEGER NOT NULL,
-    limite           NUMBER(50, 2),
+    limite           NUMBER(38, 2),
     id_conta_jur     INTEGER,
     id_conta_fisica  INTEGER
 );
@@ -49,14 +49,14 @@ ALTER TABLE cartao_debito ADD CONSTRAINT cartao_debito_pk PRIMARY KEY ( id_carta
 ALTER TABLE cartao_debito ADD CONSTRAINT debito_numero_uk UNIQUE ( numero );
 
 CREATE TABLE cliente (
-    id_cliente        NUMBER(50) NOT NULL,
-    nome              VARCHAR2(50) NOT NULL,
+    id_cliente        INTEGER NOT NULL,
+    nome              VARCHAR2(38) NOT NULL,
     sexo              VARCHAR2(9) NOT NULL,
     celular           NUMBER(15) NOT NULL,
-    email             VARCHAR2(50) NOT NULL,
-    endereco          VARCHAR2(60) NOT NULL,
-    id_cliente_jur    INTEGER,
-    id_cliente_fisica INTEGER
+    email             VARCHAR2(38) NOT NULL,
+    endereco          VARCHAR2(38) NOT NULL,
+    id_cliente_fisica INTEGER,
+    id_cliente_jur    INTEGER
 );
 
 ALTER TABLE cliente
@@ -70,10 +70,10 @@ ALTER TABLE cliente ADD CONSTRAINT cliente_pk PRIMARY KEY ( id_cliente );
 CREATE TABLE conta_corrente_fisica (
     id_cliente      INTEGER NOT NULL,
     id_conta_fisica INTEGER NOT NULL,
-    cpf             VARCHAR2(50) NOT NULL,
-    sobrenome       VARCHAR2(50) NOT NULL,
-    numero          NUMBER(50) NOT NULL,
-    saldo           NUMBER(50, 2) NOT NULL,
+    cpf             VARCHAR2(38) NOT NULL,
+    sobrenome       VARCHAR2(38) NOT NULL,
+    numero          NUMBER(38) NOT NULL,
+    saldo           NUMBER(38, 2) NOT NULL,
     status          INTEGER NOT NULL,
     data_criacao    DATE NOT NULL
 );
@@ -89,24 +89,24 @@ ALTER TABLE conta_corrente_fisica ADD CONSTRAINT id_fisica_uk UNIQUE ( id_conta_
 CREATE TABLE conta_corrente_juridica (
     id_cliente   INTEGER NOT NULL,
     id_conta_jur INTEGER NOT NULL,
-    cnpj         VARCHAR2(50) NOT NULL,
-    numero       NUMBER(50) NOT NULL,
-    saldo        NUMBER(52, 2) NOT NULL,
+    cnpj         VARCHAR2(38) NOT NULL,
+    numero       NUMBER(38) NOT NULL,
+    saldo        NUMBER(38, 2) NOT NULL,
     data_criacao DATE NOT NULL,
     status       INTEGER NOT NULL
 );
 
 ALTER TABLE conta_corrente_juridica ADD CONSTRAINT conta_juridica_pk PRIMARY KEY ( id_cliente );
 
-ALTER TABLE conta_corrente_juridica ADD CONSTRAINT conta_corrente_juridica_pk UNIQUE ( id_conta_jur );
+ALTER TABLE conta_corrente_juridica ADD CONSTRAINT conta_corrente_juridica__un UNIQUE ( cnpj );
 
-ALTER TABLE conta_corrente_juridica ADD CONSTRAINT conta_juridica_cnpj_uk UNIQUE ( cnpj );
+ALTER TABLE conta_corrente_juridica ADD CONSTRAINT conta_corrente_juridica_pk UNIQUE ( id_conta_jur );
 
 ALTER TABLE conta_corrente_juridica ADD CONSTRAINT conta_juridica_numero_un UNIQUE ( numero );
 
 CREATE TABLE emprestimos (
-    id_emprestimo        NUMBER(50) NOT NULL,
-    valor                NUMBER(50, 2) NOT NULL,
+    id_emprestimo        INTEGER NOT NULL,
+    valor                NUMBER(38, 2) NOT NULL,
     juros                NUMBER(4, 2) NOT NULL,
     data_de_realizacao   DATE NOT NULL,
     data_prazo_pagamento DATE NOT NULL,
@@ -125,13 +125,13 @@ ALTER TABLE emprestimos ADD CONSTRAINT emprestimos_pk PRIMARY KEY ( id_emprestim
 
 CREATE TABLE financiamento (
     id_financiamento INTEGER NOT NULL,
-    valor_total      NUMBER(50, 2) NOT NULL,
-    valor_parcelas   NUMBER(50, 2) NOT NULL,
+    valor_total      NUMBER(38, 2) NOT NULL,
+    valor_parcelas   NUMBER(38, 2) NOT NULL,
     qtd_parcelas     NUMBER(6) NOT NULL,
-    juros            NUMBER(50, 2) NOT NULL,
+    juros            NUMBER(38, 2) NOT NULL,
     data_realizacao  DATE NOT NULL,
     data_fim         DATE NOT NULL,
-    entrada          NUMBER(50, 2),
+    entrada          NUMBER(38, 2),
     id_conta_jur     INTEGER,
     id_conta_fisica  INTEGER
 );
@@ -145,10 +145,10 @@ ALTER TABLE financiamento
 ALTER TABLE financiamento ADD CONSTRAINT financiamento_pk PRIMARY KEY ( id_financiamento );
 
 CREATE TABLE maquininha (
-    id_maquininha   NUMBER(50) NOT NULL,
-    taxa            NUMBER(52, 2) NOT NULL,
-    vendas          NUMBER(50) NOT NULL,
-    vendas_por_mes  NUMBER(50) NOT NULL,
+    id_maquininha   INTEGER NOT NULL,
+    taxa            NUMBER(38, 2) NOT NULL,
+    vendas          NUMBER(38) NOT NULL,
+    vendas_por_mes  NUMBER(38) NOT NULL,
     id_conta_jur    INTEGER,
     id_conta_fisica INTEGER
 );
@@ -162,12 +162,12 @@ ALTER TABLE maquininha
 ALTER TABLE maquininha ADD CONSTRAINT maquininha_pk PRIMARY KEY ( id_maquininha );
 
 CREATE TABLE pix (
-    id_pix          NUMBER(50) NOT NULL,
-    chave_pix       VARCHAR2(50) NOT NULL,
-    chave_destino   VARCHAR2(50) NOT NULL,
-    limite          NUMBER(50, 2),
+    id_pix          INTEGER NOT NULL,
+    chave_pix       VARCHAR2(38) NOT NULL,
+    chave_destino   VARCHAR2(38) NOT NULL,
+    limite          NUMBER(38, 2),
     limite_horario  DATE,
-    valor_pix       NUMBER(50, 2) NOT NULL,
+    valor_pix       NUMBER(38, 2) NOT NULL,
     id_conta_jur    INTEGER,
     id_conta_fisica INTEGER
 );
@@ -181,9 +181,9 @@ ALTER TABLE pix
 ALTER TABLE pix ADD CONSTRAINT pix_pk PRIMARY KEY ( id_pix );
 
 CREATE TABLE poupanca (
-    id_poupanca     NUMBER(50) NOT NULL,
-    saldo_poupanca  NUMBER(50, 2) NOT NULL,
-    juros_poupanca  NUMBER(50, 2) NOT NULL,
+    id_poupanca     NUMBER(38) NOT NULL,
+    saldo_poupanca  NUMBER(38, 2) NOT NULL,
+    juros_poupanca  NUMBER(38, 2) NOT NULL,
     data_criacao    DATE NOT NULL,
     data_acrescimo  DATE NOT NULL,
     id_conta_jur    INTEGER,
