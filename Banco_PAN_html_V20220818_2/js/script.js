db733af434e6050901195f87501a6169f913463a
Cristian Rossmann <99340448+CristianRoss@users.noreply.github.com>
2022-09-16 12:41:00 -0300

    Delete Banco_PAN_html_V20220818_2 directory

--- Banco_PAN_html_V20220818_2/js/script.js
+++
@@ -1,7 +0,0 @@
-
-// let card = document.getElementsByClassName('card');
-// let descricao = document.getElementsByClassName('card__title');
-
-// card.addEventListener("mouseover", () => {
-//   alert("Alert");
-// });
