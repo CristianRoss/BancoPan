64a5d2f3d6af2105ca6e1250ea0670255681ccce
Cristian Rossmann <99340448+CristianRoss@users.noreply.github.com>
2022-10-07 08:38:36 -0300

    Update Bancov2.sql

--- BancoDeDados/Bancov2.sql
+++ BancoDeDados/Bancov2.sql
@@ -127,6 +127,8 @@ CREATE TABLE USUARIOS(
 
 alter table usuarios add constraint usuarios_pk primary key (id);
 
+-- PARA INSERIR: insert into usuarios(numero,senha) values (52245692,'abc');
+
 CREATE TABLE conta_poupanca (
     id_poupanca    INTEGER NOT NULL,
     numero         INTEGER NOT NULL,
